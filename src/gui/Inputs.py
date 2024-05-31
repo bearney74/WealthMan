ff7e34f615f4ed766253402f2d4fd9bec675c16e
earney <billy.earney@gmail.com>
2024-05-30 20:09:12 -0500

    Add Inputs and Analysis tab; move Input tabs to be on left side

---
+++ src/gui/Inputs.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+#from tkinter import Tk, StringVar, IntVar, Button
+from tkinter import ttk
+
+from BasicInfo import BasicInfoFrame
+from IncomeInfo import IncomeInfoFrame
+from AssetInfo import AssetInfoFrame
+
+class Inputs(tk.Frame):
+  def __init__(self, parent):
+      tk.Frame.__init__(self, parent)
+
+      #self.rowconfigure(0, weight=1)
+      #self.columnconfigure(0, weight=1)
+      style = ttk.Style(self)
+      style.configure('lefttab.TNotebook', tabposition='ws')
+      
+      tabControl = ttk.Notebook(self, style='lefttab.TNotebook')
+
+      self.Basic_tab = BasicInfoFrame(tabControl)
+      self.Income_tab = IncomeInfoFrame(tabControl)
+      Expense_tab = ttk.Frame(tabControl)
+      self.Asset_tab = AssetInfoFrame(tabControl, self.Basic_tab)
+
+      tabControl.add(self.Basic_tab, text="Basic Info")
+      tabControl.add(self.Income_tab, text="Income")
+      tabControl.add(Expense_tab, text="Expenses")
+      tabControl.add(self.Asset_tab, text="Assets")
+
+      tabControl.grid(row=0, column=0, sticky='nsew') #.pack(expand=1, fill='both')
+
+      tabControl.bind("<<NotebookTabChanged>>", self.on_tab_change)
+
+  def submit(self):
+      name=self.tk_name.get()
+
+  def on_tab_change(self, event):
+      if self.Basic_tab.status.current()==1:  #married
+         self.Asset_tab.show_spouse_frame()
+      elif self.Basic_tab.status.current()==0:
+         self.Asset_tab.hide_spouse_frame()
--- src/gui/main.py
+++ src/gui/main.py
@@ -2,9 +2,7 @@ import tkinter as tk
 #from tkinter import Tk, StringVar, IntVar, Button
 from tkinter import ttk
 
-from BasicInfo import BasicInfoFrame
-from IncomeInfo import IncomeInfoFrame
-from AssetInfo import AssetInfoFrame
+from Inputs import Inputs
 
 class App(tk.Tk):
   def __init__(self):
@@ -17,37 +15,18 @@ class App(tk.Tk):
       self.columnconfigure(0, weight=1)
       tabControl = ttk.Notebook(self)
 
-      self.Basic_tab = BasicInfoFrame(tabControl)
-      self.Income_tab = IncomeInfoFrame(tabControl)
-      Expense_tab = ttk.Frame(tabControl)
-      self.Asset_tab = AssetInfoFrame(tabControl, self.Basic_tab)
-      
-      tabControl.add(self.Basic_tab, text="Basic Info")
-      tabControl.add(self.Income_tab, text="Income")
-      tabControl.add(Expense_tab, text="Expenses")
-      tabControl.add(self.Asset_tab, text="Assets")
+      self.Inputs_tab = Inputs(tabControl)
+      self.Analysis_tab = ttk.Frame(tabControl)
+
+      tabControl.add(self.Inputs_tab, text="Inputs")
+      tabControl.add(self.Analysis_tab, text="Analysis")
 
       tabControl.grid(row=0, column=0, sticky='nsew') #.pack(expand=1, fill='both')
 
-      tabControl.bind("<<NotebookTabChanged>>", self.on_tab_change)
-      
-      
-      #tk.Button(self, text="Submit", command=self.submit).grid(row=1, column=0)
+      #tabControl.bind("<<NotebookTabChanged>>", self.on_tab_change)
 
-  def submit(self):
-      name=self.tk_name.get()
-      #print(name)
-
-  def on_tab_change(self, event):
-      #tab=event.widget.tab('current')['text']
-      #print(tab)
-      #print(self.Basic_tab.status.current())
-      if self.Basic_tab.status.current()==1:  #married
-         self.Asset_tab.show_spouse_frame()
-      elif self.Basic_tab.status.current()==0:
-         self.Asset_tab.hide_spouse_frame()
-          
-
+  
+  
 if __name__ == '__main__':
    _app=App()
    _app.mainloop()
