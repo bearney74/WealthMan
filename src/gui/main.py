840fdf786bfbdfc34019ef7be01400fad3923bef
Billy Earney <billy.earney@gmail.com>
2024-06-06 14:15:37 -0500

    add menubar

--- src/gui/main.py
+++ src/gui/main.py
@@ -1,33 +1,36 @@
 import tkinter as tk
-#from tkinter import Tk, StringVar, IntVar, Button
 from tkinter import ttk
 
 from BasicInfo import BasicInfoFrame
 from IncomeInfo import IncomeInfoFrame
+from MenuBar import MenuBar
 
 class App(tk.Tk):
   def __init__(self):
       tk.Tk.__init__(self)
 
       self.title("Wealth Manager")
-      self.geometry("400x300")
+      self.geometry("800x600")
 
+      self.menubar=MenuBar(self)
+      
       tabControl = ttk.Notebook(self)
 
       self.Basic_tab = BasicInfoFrame(tabControl)
       self.Income_tab = IncomeInfoFrame(tabControl, self.Basic_tab)
       Expense_tab = ttk.Frame(tabControl)
       Asset_tab = ttk.Frame(tabControl)
+      Global_tab = ttk.Frame(tabControl)
 
       tabControl.add(self.Basic_tab, text="Basic Info")
       tabControl.add(self.Income_tab, text="Income")
       tabControl.add(Expense_tab, text="Expenses")
       tabControl.add(Asset_tab, text="Assets")
+      tabControl.add(Global_tab, text="Global Variables")
 
       tabControl.pack(expand=1, fill='both')
 
       tabControl.bind("<<NotebookTabChanged>>", self.on_tab_change)
-      
       
       tk.Button(self, text="Submit", command=self.submit).pack()
 
@@ -45,7 +48,6 @@ class App(tk.Tk):
       elif self.Basic_tab.status==0:
          if tab == "Income":
             self.Income_tab.hide_spouse_frame()
-          
 
 if __name__ == '__main__':
    _app=App()
