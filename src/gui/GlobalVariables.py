e567db10f0521dbe42f5fd0c9abe5d95e940c0b3
bearney74 <109305761+bearney74@users.noreply.github.com>
2024-06-11 11:32:11 -0500

    Update GlobalVariables.py; remove commented out code

--- src/gui/GlobalVariables.py
+++ src/gui/GlobalVariables.py
@@ -11,22 +11,12 @@ class GlobalVariablesTab(QWidget):
       formlayout=QFormLayout()
 
       self._forecast_years=AgeEntry()   #2 digit integer
-      #self._forecast_years=QLineEdit()
-      #self._forecast_years.setMaxLength(2)
-      #self._forecast_years.setFixedWidth(30)
-      #self._forecast_years.setValidator(QIntValidator())
       formlayout.addRow(QLabel("Years to Forecast:"), self._forecast_years)
       
       self._Inflation=FloatEntry(min=-10.0, max=10.0, num_decimal_places=1)
-      #self._Inflation=QLineEdit()
-      #self._Inflation.setFixedWidth(30)
-      #self._Inflation.setValidator(QDoubleValidator(-10.0, 10.0, 1))
       formlayout.addRow(QLabel("Inflation:"), self._Inflation)
 
       self._SS_Cola=FloatEntry(min=0, max=10.0, num_decimal_places=1)
-      #self._SS_Cola=QLineEdit()
-      #self._SS_Cola.setFixedWidth(30)
-      #self._SS_Cola.setValidator(QDoubleValidator(0.0, 10.0, 1))
       formlayout.addRow(QLabel("Social Security Cola:"), self._SS_Cola)
                              
       self._WithdrawOrder=QComboBox()
