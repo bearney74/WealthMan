1df45b27007d94865eaef97342e583b4babdbef4
Billy Earney <billy.earney@gmail.com>
2026-02-13 16:43:21 -0600

    add more tests for DataItem

--- tests/DataItem_test.py
+++ tests/DataItem_test.py
@@ -9,9 +9,11 @@ class DataItemTest(unittest.TestCase):
         _a = DataItem("My header")
         self.assertEqual(str(_a), "$0")
         self.assertEqual(_a.header, "My header")
+        self.assertEqual(_a.data, 0)
         
         _a = DataItem("abc", "{:.1f}%", 123.123)
         self.assertEqual(str(_a), "123.1%")
+        self.assertEqual(_a.data, 123.123)
 
 
 if __name__ == "__main__":
